26862ab3e250db4a19808b362c0c3ffe5fadc06b
MatthewRawson <mrawson@redev.co.uk>
2024-09-30 13:43:25 +0100

    Added an override method to allow for a one off volume increase

--- WavAudioPlayer/AudioPlayer.cs
+++ WavAudioPlayer/AudioPlayer.cs
@@ -17,6 +17,7 @@ namespace WavAudioPlayer
         public const uint SND_PURGE = 0x0040;
 
         private bool disposed = false;
+        private int currentVolume = 100;
 
         /// <summary>
         /// Plays a WAV file located at the specified file path.
@@ -37,6 +38,28 @@ namespace WavAudioPlayer
             }
         }
 
+        public void PlaySound(string filepath, int volume)
+        {
+            if (string.IsNullOrEmpty(filepath))
+            {
+                throw new ArgumentException("Filepath cannot be null or empty.", nameof(filepath));
+            }
+
+            int tmpVolume = currentVolume;
+
+            SetVolume(volume);
+
+            bool result = PlaySound(filepath, IntPtr.Zero, SND_ASYNC | SND_FILENAME);
+
+            currentVolume = tmpVolume;
+
+            if (!result)
+            {
+                throw new Exception("Failed to play sound. Ensure the file path is correct and the file is a valid WAV file.");
+            }
+
+        }
+
         public void StopSound()
         {
             PlaySound(null, IntPtr.Zero, SND_PURGE);
@@ -44,9 +67,9 @@ namespace WavAudioPlayer
 
         private void SetVolume(int volume)
         {
-            volume = Clamp(volume, 0, 100);
+            currentVolume = Clamp(volume, 0, 100);
 
-            uint scaledVolume = (uint)((volume * 0xFFFF / 100) & 0xFFFF);
+            uint scaledVolume = (uint)((currentVolume * 0xFFFF / 100) & 0xFFFF);
             uint volumeAllChannels = scaledVolume | (scaledVolume << 16);
 
             WaveOutSetVolume(IntPtr.Zero, volumeAllChannels);
