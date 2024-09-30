6abf1a8cf81138a617d95f1e01db608de267bcff
MatthewRawson <mrawson@redev.co.uk>
2024-09-30 13:33:48 +0100

    Implemented IDisposable

--- WavAudioPlayer/AudioPlayer.cs
+++ WavAudioPlayer/AudioPlayer.cs
@@ -11,6 +11,9 @@ namespace WavAudioPlayer
         // Flags for playing sound
         private const uint SND_ASYNC = 0x0001;
         private const uint SND_FILENAME = 0x00020000;
+        public const uint SND_PURGE = 0x0040;
+
+        private bool disposed = false;
 
         /// <summary>
         /// Plays a WAV file located at the specified file path.
@@ -29,6 +32,31 @@ namespace WavAudioPlayer
             {
                 throw new Exception("Failed to play sound. Ensure the file path is correct and the file is a valid WAV file.");
             }
+        }
+
+        public void StopSound()
+        {
+            PlaySound(null, IntPtr.Zero, SND_PURGE);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                StopSound();
+                disposed = true;
+            }
+        }
+
+        ~AudioPlayer()
+        {
+            Dispose(false);
         }
     }
 }
